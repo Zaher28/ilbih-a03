8afc41b2b29229cd7d7c1cd1c083b04d48ffd4ea
Zaher <ilbihz@gmail.com>
2021-09-29 14:53:20 -0400

    first commit, pseudocode for ex24

---
+++ Ex24/exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Zaher Ilbih
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution24 {
+
+    /*
+    Create a program that compares two strings and determines if the two strings are anagrams.
+    The program should prompt for both input strings and display the output as shown in the example that follows.
+     */
+
+    //method that takes in two Strings and compares them to see if they are anagrams of each other
+    //returns true if they are anagrams and false if they are not
+    public boolean isAnagram(String word1, String word2){
+
+
+    }
+
+    //main method
+    public static void main(String[] args){
+
+
+
+    }
+
+
+}
---
+++ Ex24/exercise24/src/test/java/baseline/Solution24Test.java
@@ -0,0 +1,17 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution24Test {
+
+    Solution24 sol = new Solution24();
+
+    @Test
+    void isAnagram(){
+        assertEquals(true, sol.isAnagram("antler", "learnt"));
+    }
+
+
+}
