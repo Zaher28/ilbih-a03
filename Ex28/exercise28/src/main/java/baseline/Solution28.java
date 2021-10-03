a73b186b6892f8b48fc140e7f7ba5df5d3e4d90e
Zaher <ilbihz@gmail.com>
2021-10-02 21:37:57 -0400

    pseudocode ex28

---
+++ Ex28/exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,26 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution28 {
+
+    /*
+    Write a program that prompts the user for five numbers
+     and computes the total of the numbers.
+     */
+
+    //method that sums up 5 numbers with a for loop
+    public double sumNumbers(){
+
+        
+
+    }
+
+    //Scanner object for input
+    public static final Scanner scan = new Scanner(System.in);
+
+    //main method
+    public static void main(String[] args){
+
+    }
+}
---
+++ Ex28/exercise28/src/test/java/baseline/Solution28Test.java
@@ -0,0 +1,16 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution28Test {
+
+    Solution28 sol = new Solution28();
+
+    @Test
+    void sumNumbers() {
+
+        assertEquals(15,sol.sumNumbers());
+    }
+}
