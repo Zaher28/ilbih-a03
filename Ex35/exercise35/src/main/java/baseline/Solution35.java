80542fe09db3dce693482dbc81d079c795339204
Zaher <ilbihz@gmail.com>
2021-10-03 12:11:12 -0400

    pseudocode for ex35

---
+++ Ex35/exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,30 @@
+package baseline;
+
+//for scanner and arrayList
+import java.util.*;
+
+public class Solution35 {
+
+
+    //method that will pick a winner out of a bunch of names in the list
+    public void pickAWinner(List<String> names){
+
+    }
+    //method that will take in all names and put them into a list
+    public void takeInInput(List<String> names){
+
+    }
+
+    //Scanner object for input
+    public static final Scanner scan = new Scanner (System.in);
+
+    //main method
+    public static void main(String[] args){
+
+        Solution35 sol = new Solution35();
+
+        //list to hold in the names
+        List<String> names = new ArrayList<>();
+
+    }
+}
