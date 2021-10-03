9a34d3fd9bd95a0ab2c66063ad246123a720327d
Zaher <ilbihz@gmail.com>
2021-10-03 12:48:26 -0400

    pseudocode for ex36

---
+++ Ex36/exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,47 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Zaher Ilbih
+ */
+package baseline;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution36 {
+
+    /*
+    Write a program that prompts for response times from a website in milliseconds.
+    It should keep asking for values until the user enters “done.”
+    The program should print the average time (mean),
+    the minimum time, the maximum time, and the population standard deviation.
+     */
+
+    //method that takes in a list of numbers and returns the average
+    public double average(List<Integer> numbers){
+
+    }
+
+    //method that takes in a list of numbers and returns the biggest number
+    public int max(List<Integer> numbers){
+
+    }
+
+    //method that takes in a list of numbers and returns the smallest number
+    public int min(List<Integer> numbers){
+
+    }
+
+    //method that takes in a list of numbers and returns the standard deviation of the numbers
+    public double std(List<Integer> numbers){
+
+    }
+
+    //Scanner object for inputs
+    public static final Scanner scan = new Scanner(System.in);
+
+    //main method
+    public static void main(String[] args){
+
+    }
+}
---
+++ Ex36/exercise36/src/test/java/baseline/Solution36Test.java
@@ -0,0 +1,30 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution36Test {
+
+    Solution36 sol = new Solution36();
+    List<Integer> numbers = new ArrayList<>();
+    
+    @Test
+    void average() {
+    }
+
+    @Test
+    void max() {
+    }
+
+    @Test
+    void min() {
+    }
+
+    @Test
+    void std() {
+    }
+}
