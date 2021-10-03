a0ce264239f7095f10b02ec810147262ed8d2622
Zaher <ilbihz@gmail.com>
2021-10-03 18:43:38 -0400

    finished ex38

--- Ex38/exercise38/src/main/java/baseline/Solution38.java
+++ Ex38/exercise38/src/main/java/baseline/Solution38.java
@@ -12,14 +12,82 @@ public class Solution38 {
     public int[] convertStringToIntArray(String numbers){
 
 
+        //have temp variable be start of string
+        char temp=numbers.charAt(0);
+        int i=0;
+        int countSpaces=0;
+        String sub=numbers;
+        int j=0;
 
+        //count spaces in string to know size of future array
+        while(sub.indexOf(' ')!=-1){
+
+            sub= sub.substring(sub.indexOf(' ') + 1);
+            countSpaces++;
+
+        }
+
+        //new array
+        int[] arr = new int[countSpaces+1];
+
+        //get all the numbers separated by white spaces
+        while(i<numbers.length()){
+
+            if(temp == ' '){
+                i++;
+                temp = numbers.charAt(i);
+                continue;
+            }
+
+            String num = "";
+            while(temp!=' ' && i<numbers.length()){
+
+                num+=temp;
+                i++;
+                if(i<numbers.length())
+                    temp=numbers.charAt(i);
+
+
+
+            }
+
+            arr[j] = Integer.parseInt(num);
+            j++;
+            i++;
+            if(i<numbers.length())
+                temp = numbers.charAt(i);
+
+
+
+        }
+
+        return arr;
     }
 
     //method that takes in an array of numbers and
     // returns an array with only the even ones
     public int[] filterEvenNumbers(int[] numbers){
 
+        int count=0;
+        int j=0;
 
+        for(int i: numbers){
+            if(i%2 == 0)
+                count++;
+        }
+
+        int[] even = new int[count];
+
+        for(int i =0;i<numbers.length;i++){
+            if(numbers[i]%2 == 0){
+                even[j]=numbers[i];
+                j++;
+            }
+
+
+        }
+
+        return even;
 
     }
 
@@ -34,6 +102,16 @@ public class Solution38 {
         //ask for numbers and store them in String to later parse into array
         System.out.print("Enter a list of numbers, separated by spaces: ");
         String numbers = scan.nextLine();
+
+        int[] arr = sol.convertStringToIntArray(numbers);
+        int[] even = sol.filterEvenNumbers(arr);
+
+        System.out.print("The even numbers are");
+        for(int i: even){
+            System.out.print(" " + i);
+        }
+        System.out.println(".");
+
 
 
 
