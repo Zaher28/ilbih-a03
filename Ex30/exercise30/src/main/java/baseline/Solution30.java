ecc98156c1bc8db7955477138e68931db9b95be4
Zaher <ilbihz@gmail.com>
2021-10-03 00:28:21 -0400

    finished ex30

--- Ex30/exercise30/src/main/java/baseline/Solution30.java
+++ Ex30/exercise30/src/main/java/baseline/Solution30.java
@@ -10,10 +10,26 @@ public class Solution30 {
     //method that generates a multiplication table
     public void generateMultiplicationTable(){
 
+        for(int i=1;i<=12;i++){
+
+            for(int j = 1;j<=12;j++){
+
+                System.out.printf("%5s   ",i*j);
+
+
+            }
+
+            System.out.println();
+
+        }
     }
 
     //main method
     public static void main(String[] args){
+
+        Solution30 sol = new Solution30();
+
+        sol.generateMultiplicationTable();
 
     }
 
