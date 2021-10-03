10667d4832554cd1fafa9eec8f45d27771a3b1a5
Zaher <ilbihz@gmail.com>
2021-10-03 00:08:18 -0400

    first commit for ex30

---
+++ Ex30/exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,20 @@
+package baseline;
+
+public class Solution30 {
+
+    /*
+    Create a program that generates a multiplication
+     table for the numbers 1 through 12 (inclusive).
+     */
+
+    //method that generates a multiplication table
+    public void generateMultiplicationTable(){
+
+    }
+
+    //main method
+    public static void main(String[] args){
+
+    }
+
+}
