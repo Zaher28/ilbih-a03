592923d2208940c185cb85dc853d59fdcff51fec
Zaher <ilbihz@gmail.com>
2021-10-03 11:40:47 -0400

    pseudocode for ex34

---
+++ Ex34/exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Zaher Ilbih
+ */
+package baseline;
+
+public class Solution34 {
+
+    /*
+    Create a small program that contains a list of employee names.
+    Print out the list of names when the program runs the first time.
+    Prompt for an employee name and remove that specific name from the list of names.
+    Display the remaining employees, each on its own line.
+     */
+
+    //method that takes in the list of names and the name to remove and removes it
+    public String[] removeEmployeeFromList(String[] list, String nameToRemove){
+
+    }
+
+    //main method
+    public static void main(String[] args){
+
+        //list of employees
+        String[] list;
+        list = new String[]{"John Smith","Jackie Jackson", "Chris Jones","Amanda Cullen", "Jeremy Goodwin"};
+
+        
+    }
+
+}
