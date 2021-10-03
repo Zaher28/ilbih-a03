8db99dfd460984dd81e6bdf8b42ea07129430ad8
Zaher <ilbihz@gmail.com>
2021-10-03 12:00:47 -0400

    finished ex34

--- Ex34/exercise34/src/main/java/baseline/Solution34.java
+++ Ex34/exercise34/src/main/java/baseline/Solution34.java
@@ -7,6 +7,7 @@ package baseline;
 //to use a list
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Solution34 {
 
@@ -20,19 +21,44 @@ public class Solution34 {
     //method that takes in the list of names and the name to remove and removes it
     public void removeEmployeeFromList(List<String> employees, String nameToRemove){
 
+        employees.remove(nameToRemove);
+
     }
+    //Scanner object for input
+    public static final Scanner scan = new Scanner(System.in);
 
     //main method
     public static void main(String[] args){
 
-        //list of employees
-       List<String> employees = new ArrayList<String>();
-       employees.add("John Smith");
-       employees.add("Jackie Jackson");
-       employees.add("Chris Jones");
-       employees.add("Amanda Cullen");
-       employees.add("Jeremy Goodwin");
+        Solution34 sol = new Solution34();
 
+        //list of employees
+        List<String> employees = new ArrayList<>();
+        employees.add("John Smith");
+        employees.add("Jackie Jackson");
+        employees.add("Chris Jones");
+        employees.add("Amanda Cullen");
+        employees.add("Jeremy Goodwin");
+
+        System.out.println("There are 5 employees:");
+
+        //for each to go through and print all employees
+        for (String employee : employees) {
+            System.out.printf("%s\n", employee);
+        }
+
+        //remove employee and spacing
+        System.out.print("\nEnter an employee name to remove: ");
+        String nameToRemove = scan.nextLine();
+        System.out.println();
+
+        sol.removeEmployeeFromList(employees, nameToRemove);
+
+        //for each to go through and print all remaining employees
+        System.out.println("There are 4 employees:");
+        for(String employee: employees){
+            System.out.printf("%s\n",employee);
+        }
 
     }
 
