5fa270fefba054c757fd09ed6ef3b0e63a3a0bff
Zaher <ilbihz@gmail.com>
2021-10-03 18:11:23 -0400

    pseudocode for ex38

---
+++ Ex38/exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Zaher Ilbih
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution38 {
+
+
+    //method that takes in an array of numbers and
+    // returns an array with only the even ones
+    public int[] filterEvenNumbers(int[] numbers){
+
+    }
+
+    //Scanner object for input
+    public static final Scanner scan = new Scanner(System.in);
+
+    //main method
+    public static void main(String[] args){
+
+        //ask for numbers and store them in String to later parse into array
+        System.out.print("Enter a list of numbers, separated by spaces: ");
+
+        
+    }
+}
