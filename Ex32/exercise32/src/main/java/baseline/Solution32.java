2ef5febdaa43bb6a8d05a7542bf80649ea978853
Zaher <ilbihz@gmail.com>
2021-10-03 10:26:58 -0400

    pseudocode for ex32

---
+++ Ex32/exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,27 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution32 {
+
+    /*
+    Write a Guess the Number game that has three levels of difficulty.
+    The first level of difficulty would be a number between 1 and 10.
+    The second difficulty set would be between 1 and 100.
+    The third difficulty set would be between 1 and 1000.
+     */
+
+    //method that lets the user play the Guess the number game
+    public void guessTheNumber(int difficulty){
+
+
+
+    }
+
+    //Scanner object for input
+    public static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+    }
+}
