7fbce87d1adb4ae301ee50d3d90500ac24bcaa85
Zaher <ilbihz@gmail.com>
2021-10-03 11:22:03 -0400

    pseudocode for ex33

---
+++ Ex33/exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Zaher Ilbih
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution33 {
+
+    /*
+    Create a Magic 8 Ball game that prompts for a question and
+     then displays either "Yes," "No," "Maybe," or "Ask again later."
+     */
+
+    //method which takes in a list of options, then prompts the user for a question
+    //and prints out a response from the "Magic 8 Ball"
+    public void shakeMagic8Ball(int[] arr){
+
+    }
+
+    //Scanner object for input
+    public static final Scanner scan = new Scanner (System.in);
+
+    //main method
+    public static void main(String[] args){
+
+
+
+    }
+}
