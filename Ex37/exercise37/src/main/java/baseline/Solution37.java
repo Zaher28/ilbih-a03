9bb3780d200214fcdc8025d368a677aae6ff2c37
Zaher <ilbihz@gmail.com>
2021-10-03 15:39:41 -0400

    pseudocode for ex37

---
+++ Ex37/exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,28 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution37 {
+
+    /*
+    Create a program that generates a secure password.
+    Prompt the user for the minimum length, the number of special characters, and the number of numbers.
+     Then generate a password for the user using those inputs.
+     */
+
+    //method that takes in the minimum length of the password,
+    // how many special characters, and how many numbers are in the password
+    public String generatePassword(int minLen,int specialChars, int numbers){
+
+    }
+
+    //Scanner object for taking input
+    public static final Scanner scan = new Scanner(System.in);
+
+    //main method
+    public static void main(String[] args){
+
+    }
+
+
+}
