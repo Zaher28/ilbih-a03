8bbf366cd7641ca7e68719caf49d7bea318a1c8d
Zaher <ilbihz@gmail.com>
2021-10-03 12:19:09 -0400

    finished ex35

--- Ex35/exercise35/src/main/java/baseline/Solution35.java
+++ Ex35/exercise35/src/main/java/baseline/Solution35.java
@@ -9,9 +9,26 @@ public class Solution35 {
     //method that will pick a winner out of a bunch of names in the list
     public void pickAWinner(List<String> names){
 
+        int rand =(int) (Math.random()*names.size());
+
+        System.out.printf("The winner is... %s",names.get(rand));
+
     }
     //method that will take in all names and put them into a list
     public void takeInInput(List<String> names){
+
+        String name="";
+        //loop that takes in input until a blank is reached
+        do{
+
+            System.out.print("Enter a name: ");
+            name = scan.nextLine();
+
+            if(!name.equals(""))
+                names.add(name);
+
+        }while(!name.equals(""));
+
 
     }
 
@@ -25,6 +42,10 @@ public class Solution35 {
 
         //list to hold in the names
         List<String> names = new ArrayList<>();
+
+        //take in input and pick a winner
+        sol.takeInInput(names);
+        sol.pickAWinner(names);
 
     }
 }
